5aa09bb6f116dfbdef22adbce699a317fd87992d
Tobias Bohwalli <hi@futhr.io>
2026-04-03 01:08:31 +0200

    refactor: move git_ops config to dev environment

--- config/config.exs
+++ config/config.exs
@@ -16,15 +16,6 @@ config :logger,
   level: :warning,
   format: "[$level] $message\n"
 
-# Git Ops - automated changelog and version management
-config :git_ops,
-  mix_project: Mix.Project.get!(),
-  changelog_file: "CHANGELOG.md",
-  repository_url: "https://github.com/futhr/ash_oaskit",
-  version_tag_prefix: "v",
-  manage_mix_version?: true,
-  manage_readme_version: "README.md"
-
 # Import environment specific config
 if File.exists?("config/#{config_env()}.exs") do
   import_config "#{config_env()}.exs"
