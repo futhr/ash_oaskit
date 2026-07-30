91f5f423679eee2157bc1ec38ce31dcd23ff7407
futhr <hi@futhr.io>
2026-07-30 12:37:10 +0200

    chore: require Elixir 1.17

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule AshOaskit.MixProject do
     [
       app: :ash_oaskit,
       version: @version,
-      elixir: "~> 1.16",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [
         no_warn_undefined: [AshJsonApi.Domain.Info, AshJsonApi.Resource.Info]
