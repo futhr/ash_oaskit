a9985d1a928e23a5380655b186fc3b28a1d0826e
Tobias Bohwalli <hi@futhr.io>
2026-09-06 06:01:47 +0200

    test(credo): cover nesting boundaries for all control constructs

--- test/quality/credo_nesting_test.exs
+++ test/quality/credo_nesting_test.exs
@@ -69,6 +69,32 @@ defmodule AshOaskit.Quality.CredoNestingTest do
     assert message =~ "max depth is 2, was 3"
   end
 
+  for {construct, expression} <- [
+        {"unless", "unless false, do: :ok"},
+        {"cond", "cond do\ntrue -> :ok\nend"},
+        {"for", "for item <- [1], do: item"},
+        {"with", "with {:ok, value} <- {:ok, 1}, do: value"}
+      ] do
+    test "counts #{construct} at both sides of the nesting boundary", %{params: params} do
+      assert issues(nested_fixture(unquote(expression), 1), params) == []
+
+      assert [%{message: message}] = issues(nested_fixture(unquote(expression), 2), params)
+      assert message =~ "max depth is 2, was 3"
+    end
+  end
+
+  defp nested_fixture(expression, depth) do
+    body = Enum.reduce(1..depth, expression, fn _, body -> "if true do\n#{body}\nend" end)
+
+    """
+    defmodule NestingFixture do
+      def sample do
+        #{body}
+      end
+    end
+    """
+  end
+
   defp issues(source, params) do
     source
     |> SourceFile.parse("nesting_fixture.ex")
